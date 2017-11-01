6b147aaa8c1507c5c8cc1a3f830f17b764d0c076
Mickaël Bernardini <contact@mickael-bernardini.fr>
2017-11-01 11:24:18 +0100

    Start business package

---
+++ DirectoryManagement/src/business/User.java
@@ -0,0 +1,15 @@
+package business;
+
+public class User {
+
+	private boolean anonymous = true;
+
+	public boolean isAnonymous() {
+		return anonymous;
+	}
+
+	public void setAnonymous(boolean anonymous) {
+		this.anonymous = anonymous;
+	}
+
+}

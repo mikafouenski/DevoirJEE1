a40973ca741a3be6d1b7623c2f766d8f55c789a3
sylvain099 <sylvain099@hotmail.fr>
2017-12-05 19:54:47 +0100

    testDirectoryManager

--- DirectoryManagement/test/business/TestDirectoryManager.java
+++ DirectoryManagement/test/business/TestDirectoryManager.java
@@ -13,6 +13,7 @@ import beans.Person;
 import business.IDirectoryManager;
 import business.User;
 import database.IDaoPerson;
+import business.exception.PersonNotFoundException;
 import business.exception.UserNotLoggedException;
 import mockit.Expectations;
 import mockit.Injectable;
@@ -58,9 +59,120 @@ public class TestDirectoryManager {
         new Verifications() {{ dao.findGroup(1); times = 1; }};
     }
     
+    @Test
+    public void testFindAllGroup() throws UserNotLoggedException {
+        new Expectations() {{
+            dao.findAllPersons(1); result = new Group();
+        }};
+        User login = new User();
+        login.setAnonymous(false);
+        manager.findAllPersons(login,1);
+        new Verifications() {{ dao.findAllPersons(1); times = 1; }};
+    }
+    
+    @Test
+    public void testLoginIsLog() throws PersonNotFoundException {
+        new Expectations() {{
+        	Person p = new Person();
+        	p.setPassword("juste");
+        	p.setIdGroup(12L);
+            dao.findPerson(1); result = p;
+        }};
+        User user = new User();
+        manager.login(user, 1, "juste");
+        assertFalse(user.isAnonymous());
+        new Verifications() {{ dao.findPerson(1); times = 1; }};
+    }
+    
+    @Test
+    public void testLoginGroup() throws PersonNotFoundException {
+        new Expectations() {{
+        	Person p = new Person();
+        	p.setPassword("juste");
+        	p.setIdGroup(12L);
+            dao.findPerson(1); result = p;
+        }};
+        User user = new User();
+        manager.login(user, 1, "juste");
+        assertEquals(12L, user.getGroupId());
+        new Verifications() {{ dao.findPerson(1); times = 1; }};
+    }
+    
+    @Test
+	public void testLogout() {
+    	User test = new User();
+    	test.setAnonymous(false);
+    	manager.logout(test);
+    	assertTrue(test.isAnonymous());
+	}
+    
+    @Test
+  	public void testLogoutOnAnonymous() {
+      	User test = new User();
+      	test.setAnonymous(true);
+      	manager.logout(test);
+      	assertTrue(test.isAnonymous());
+  	}
+    
+    @Test
+  	public void testSavePerson() throws UserNotLoggedException {
+    	Person p = new Person();
+    	new Expectations() {{
+            dao.savePerson(p); 
+        }};
+    	User user = new User();
+    	user.setAnonymous(false);
+    	manager.savePerson(user, p);
+    	new Verifications() {{  dao.savePerson(p); times = 1; }};
+    }
+    
+    
+    @Test
+    public void testLoginFail() throws PersonNotFoundException {
+        new Expectations() {{
+        	Person p = new Person();
+        	p.setPassword("juste");
+        	p.setIdGroup(12L);
+            dao.findPerson(1); result = p;
+        }};
+        User user = new User();
+        boolean isCo = manager.login(user, 1, "false");
+        assertFalse(isCo && !user.isAnonymous());
+        new Verifications() {{ dao.findPerson(1); times = 1; }};
+    }
+    
+    
+    
+    
+    @Test(expected = PersonNotFoundException.class)
+    public void testLoginFailId() throws PersonNotFoundException {
+        new Expectations() {{;
+            dao.findPerson(1); result = null;
+        }};
+        User user = new User();
+        manager.login(user, 1, "false");
+        new Verifications() {{ dao.findPerson(1); times = 1; }};
+    }
+    
+    @Test(expected = UserNotLoggedException.class)
+    public void testsavePersonailled() throws UserNotLoggedException {
+        manager. savePerson(new User(),new Person());
+    }
+   
+    
     @Test(expected = UserNotLoggedException.class)
     public void testFindGroupFailled() throws UserNotLoggedException {
         manager.findGroup(new User(),1);
+    }
+    
+    @Test(expected = UserNotLoggedException.class)
+    public void testFindPersonFailled() throws UserNotLoggedException {
+        manager.findPerson(new User(),1);
+    }
+    
+    @Test(expected = UserNotLoggedException.class)
+    public void testFindAllPersonFailled() throws UserNotLoggedException {
+        manager.findAllPersons(new User(),1);
     }
     
 }

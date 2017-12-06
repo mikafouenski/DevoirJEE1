a24b94b60f33d60f4fa695239e8bf56e30c24b2c
sylvain099 <sylvain099@hotmail.fr>
2017-12-06 17:51:22 +0100

    Petit test

---
+++ DirectoryManagement/test/tomcatControleur/TestGroupController.java
@@ -0,0 +1,53 @@
+package tomcatControleur;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.servlet.ModelAndView;
+
+import beans.Person;
+import business.IDirectoryManager;
+import business.User;
+import business.exception.UserNotLoggedException;
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Tested;
+import mockit.Verifications;
+import web.GroupController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/spring.xml")
+public class TestGroupController {
+	
+    @Tested
+    GroupController groupController;
+    
+    @Injectable
+    IDirectoryManager manager;
+
+    @Test
+    public void testlistGroup() throws UserNotLoggedException {
+    	User user = new User();
+    	user.setAnonymous(false);
+    	Person toto = new Person();
+    	Collection<Person> persons = new ArrayList<Person>();
+    	persons.add(toto);
+    	new Expectations() {{
+        	manager.findAllPersons(user,1); result = persons;
+        }};
+        ModelAndView result = groupController.listGroup(1);     
+        assertEquals( result.getView().toString(),"listPersons");
+        new Verifications() {{ manager.findAllPersons(user,1); times = 1; }};
+    }
+    
+    
+    
+}

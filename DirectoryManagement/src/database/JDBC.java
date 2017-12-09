1357bf593f3d6c59795e39c63bc8253d04b37c5e
Mickaël Bernardini <contact@mickael-bernardini.fr>
2017-12-09 12:28:31 +0100

    Fix Jdbc with alwaysdata

--- DirectoryManagement/src/database/JDBC.java
+++ DirectoryManagement/src/database/JDBC.java
@@ -14,7 +14,7 @@ public class JDBC implements IDatabase {
 	
 	private static final int MAX_CONNEXION = 5;
 
-	private String url = "mysql-mickaestnul.alwaysdata.net/mickaestnul_b13";//"jdbc:mysql://127.0.0.1:9999/b13002175";
+	private String url = "jdbc:mysql://mysql-mickaestnul.alwaysdata.net/mickaestnul_b13";//"jdbc:mysql://127.0.0.1:9999/b13002175";
 	private String id = "148878"; //b13002175";
 	private String password = "root";//"gRAYUM!.";
 	private BasicDataSource bds;

5918781b748ccb7e0e6f11909108dce5c5a21150
sylvain099 <sylvain099@hotmail.fr>
2017-12-09 21:39:02 +0100

    Test ConnecControlleur

--- DirectoryManagement/test/tomcatControleur/TestGroupController.java
+++ DirectoryManagement/test/tomcatControleur/TestGroupController.java
@@ -1,16 +1,14 @@
 package tomcatControleur;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.ArrayList;
 import java.util.Collection;
 
-import org.junit.Ignore;
+import javax.servlet.http.HttpServletRequest;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.servlet.ModelAndView;
 
 import beans.Person;
 import business.IDirectoryManager;
@@ -33,19 +31,23 @@ public class TestGroupController {
     IDirectoryManager manager;
 
 //    @Test
-//    @Ignore
 //    public void testlistGroup() throws UserNotLoggedException {
 //    	User user = new User();
 //    	user.setAnonymous(false);
 //    	Person toto = new Person();
 //    	Collection<Person> persons = new ArrayList<Person>();
 //    	persons.add(toto);
+//    	HttpServletRequest r;
 //    	new Expectations() {{
-//        	manager.findPersons(user,1); result = persons;
-//        }};
+//    		r.getSession().getAttribute("user"); result = user;
+//    		manager.findPersons(user,1); result = persons;
+//        	
+//    	}};
 //        ModelAndView result = groupController.listGroup(1);     
 //        assertEquals( result.getView().toString(),"listPersons");
 //        new Verifications() {{ manager.findPersons(user,1); times = 1; }};
 //    }
-  
+    
+    	
+    
 }

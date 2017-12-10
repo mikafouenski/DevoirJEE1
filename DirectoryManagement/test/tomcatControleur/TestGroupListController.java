44d9a3507ddb323c2fbd4d4b7b8f1fffcb8fb2a2
sylvain099 <sylvain099@hotmail.fr>
2017-12-10 11:12:45 +0100

    test ControleurGroupList

--- DirectoryManagement/test/tomcatControleur/TestGroupListController.java
+++ DirectoryManagement/test/tomcatControleur/TestGroupListController.java
@@ -1,41 +1,105 @@
 package tomcatControleur;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import beans.Group;
 import business.IDirectoryManager;
+import business.User;
+import business.exception.UserNotLoggedException;
+import mockit.Expectations;
 import mockit.Injectable;
+import mockit.Mocked;
 import mockit.Tested;
+import web.ControllerHelpers;
 import web.group.GroupListController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/spring.xml")
-public class TestGroupController {
+public class TestGroupListController {
 	
     @Tested
     GroupListController groupController;
     
     @Injectable
     IDirectoryManager manager;
+    
+    @Mocked
+	HttpServletRequest request;
+	
+	@Mocked
+	HttpServletResponse response;
+	
+	
+	@Test
+    public void testListGroup() throws UserNotLoggedException {
+		Collection<Group> groupsExpected = new ArrayList<Group>();
+		Group g = new Group();
+		g.setId(1L);
+		g.setName("test");
+		Group g2 = new Group();
+		g2.setId(1L);
+		g2.setName("test");
+		groupsExpected.add(g);
+		groupsExpected.add(g2);
+		User user = new User();
+		user.setAnonymous(false);
+		new Expectations() {{
+			request.getSession().getAttribute("user"); result = user;
+			manager.nbGroups(user); result = 3;
+			manager.findGroups(user,anyInt, anyInt); result = groupsExpected; 
+    	}};
+    	ModelAndView actual = groupController.listGroup(0,7,request,response);
+    	assertEquals("group/listGroups", actual.getViewName());
+    }
+    
+	@Test
+    public void testListGroupReturnGroup() throws UserNotLoggedException {
+		Collection<Group> groupsExpected = new ArrayList<Group>();
+		Group g = new Group();
+		g.setId(1L);
+		g.setName("test");
+		Group g2 = new Group();
+		g2.setId(1L);
+		g2.setName("test");
+		groupsExpected.add(g);
+		groupsExpected.add(g2);
+		User user = new User();
+		user.setAnonymous(false);
+		new Expectations() {{
+			request.getSession().getAttribute("user"); result = user;
+			manager.nbGroups(user); result = 3;
+			manager.findGroups(user,anyInt, anyInt); result = groupsExpected; 
+    	}};
+    	ModelAndView actual = groupController.listGroup(0,7,request,response);
+    	assertEquals(actual.getModelMap().get("groups"), groupsExpected);
+    }
+    
+	@Test
+    public void testListGroupUserBad() throws UserNotLoggedException {
+		new Expectations() {{
+		    request.getSession().getAttribute("user"); result = null;
+		    manager.nbGroups((User)any); result = new UserNotLoggedException();
+    	}};
+    	ModelAndView actual = groupController.listGroup(0,7,request,response);
+    	assertEquals(actual.getViewName(), "redirect:/login");
+    }
+   
+	
 
-//    @Test
-//    public void testlistGroup() throws UserNotLoggedException {
-//    	User user = new User();
-//    	user.setAnonymous(false);
-//    	Person toto = new Person();
-//    	Collection<Person> persons = new ArrayList<Person>();
-//    	persons.add(toto);
-//    	HttpServletRequest r;
-//    	new Expectations() {{
-//    		r.getSession().getAttribute("user"); result = user;
-//    		manager.findPersons(user,1); result = persons;
-//        	
-//    	}};
-//        ModelAndView result = groupController.listGroup(1);     
-//        assertEquals( result.getView().toString(),"listPersons");
-//        new Verifications() {{ manager.findPersons(user,1); times = 1; }};
-//    }
     
     	
     
